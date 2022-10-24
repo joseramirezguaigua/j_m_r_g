a0505c0f4d66c55b4a1c5a4abe79ea8f428c8581
José Ramírez Guaigua <66870805+joseramirezguaigua@users.noreply.github.com>
2022-10-23 21:30:05 -0400

    Update sketch.js

--- 7200_variaciones_en_blanco_rojo_y_negro/sketch.js
+++ 7200_variaciones_en_blanco_rojo_y_negro/sketch.js
@@ -31,7 +31,7 @@ window.setTimeout( function() {
 
 function preload() {
   myText = loadStrings("3umbral_.txt");
-  myFont = loadFont("Jura-Bold.ttf");
+  myFont = loadFont("https://fonts.googleapis.com/css2?family=Jura:wght@700&display=swap");
 }
 
 function setup() {
